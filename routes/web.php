cf57a82ae2528954131b311eb0c1b6569a2eb11c
Czar <czarbasanal@gmail.com>
2024-06-20 10:33:06 +0800

    push .env

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,13 @@ Route::get('/', function () {
 
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
 Route::get('/logout', [AuthController::class, 'logout']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::view('/home', 'homepage');
+});
